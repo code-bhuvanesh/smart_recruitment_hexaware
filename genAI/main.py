61a31e2e43273d56bd5c3bb53da726fa196022c3
pbharathcr7 <pbharathcr7@gmail.com>
2024-09-02 21:32:18 +0530

    commit

--- genAI/main.py
+++ genAI/main.py
@@ -3,23 +3,29 @@ import streamlit as st
 from app import chatbotApp
 from job_des_ui import jobDescriptionApp
 from resume_analyzer_ui import resumeAnlayzerApp
+from streamlit_option_menu import option_menu   
 # from resume_analayzer.resume_analyzer import ResumeAnalyzer
 # from job_description.job_description_ai import jobDescriptionAI
 
-# Set the page configuration
 st.set_page_config(page_title="Smart Recruitment App", page_icon=":rocket:", layout="centered")
 
-# Title of the main page
-st.title("Welcome to the Smart Recruitment App")
+st.title("Smart Recruitment App")
 
-# Sidebar or Main Page Navigation
-st.sidebar.title("Navigation")
-option = st.sidebar.radio("Go to", ("Job Description", "Resume Analyzer", "Chatbot"))
 
-# Conditional rendering based on user selection
-if option == "Job Description":
+with st.sidebar:
+    selected = option_menu(
+        menu_title="",
+        options=["Job Description", "Resume Analyzer","Chatbot"],
+        icons=["bi bi-file-earmark-text-fill", "bi bi-bar-chart-line-fill","bi bi-chat-fill"],
+        menu_icon="",
+        default_index=0,
+    )
+
+if selected == "Job Description":
     jobDescriptionApp()
-elif option == "Resume Analyzer":
-    resumeAnlayzerApp() # Call the function from resume_analyzer_app
-elif option == "Chatbot":
-    chatbotApp()  # Call the function from chatbot_app
+
+if selected == "Resume Analyzer":
+    resumeAnlayzerApp()
+
+if selected == "Chatbot":
+    chatbotApp()
