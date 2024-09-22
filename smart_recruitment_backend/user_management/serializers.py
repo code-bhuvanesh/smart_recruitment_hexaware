d4db481499a1ade87564c4cb5bf77aa1a256ee9f
Bhuvanesh <90144561+code-bhuvanesh@users.noreply.github.com>
2024-09-22 15:19:53 +0530

    commit

--- smart_recruitment_backend/user_management/serializers.py
+++ smart_recruitment_backend/user_management/serializers.py
@@ -1,45 +1,92 @@
-# accounts/serializers.py
+# user_management/serializers.py
 
+from datetime import timezone
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from .models import RecruiterProfile, ApplicantProfile
+from .models import CustomUser, Applicant, Recruiter, PostedJob
+from django.contrib.auth import authenticate
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
+class ApplicantSignupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password']
+        model = CustomUser
+        fields = ['username', 'password', 'email', 'usertype']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
+        user = CustomUser(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            usertype='applicant'
+        )
+        user.set_password(validated_data['password'])
         user.save()
+        applicant = Applicant.objects.create(user=user, skills=validated_data.get('skills', ''))
         return user
 
-class RecruiterProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
 
+class RecruiterSignupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = RecruiterProfile
-        fields = ['user']
+        model = CustomUser
+        fields = ['username', 'password', 'email', 'usertype']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
-        recruiter_profile = RecruiterProfile.objects.create(user=user, **validated_data)
-        return recruiter_profile
+        user = CustomUser(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            usertype='recruiter'
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        recruiter = Recruiter.objects.create(user=user, company_name=validated_data.get('company_name', ''))
+        return user
 
-class ApplicantProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
 
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is None:
+                raise serializers.ValidationError("Invalid credentials.")
+        else:
+            raise serializers.ValidationError("Must include username and password.")
+        
+        attrs['user'] = user
+        return attrs
+
+
+
+class PostedJobSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ApplicantProfile
-        fields = ['user', 'resume', 'skills']
+        model = PostedJob
+        fields = [
+            'user',  # Include user field
+            'job_location',
+            'employment_type',
+            'salary_range',
+            'application_deadline',
+            'required_qualifications',
+            'preferred_qualifications',
+            'responsibilities'
+        ]
+        read_only_fields = ('user',)  # Prevent users from setting this field
 
     def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
-        applicant_profile = ApplicantProfile.objects.create(user=user, **validated_data)
-        return applicant_profile
+        user = validated_data.pop('user')  # Extract user info
+        job = PostedJob.objects.create(user=user, **validated_data)
+        return job
+    
+    def validate_application_deadline(self, value):
+        if value < timezone.now().date():
+            raise serializers.ValidationError("Application deadline must be a future date.")
+        return value
+
+    def validate_salary_range(self, value):
+        if not value.replace('.', '', 1).isdigit():
+            raise serializers.ValidationError("Salary range must be numeric values.")
+        return value
--- smart_recruitment_backend/user_management/views.py
+++ smart_recruitment_backend/user_management/views.py
@@ -1,46 +1,44 @@
-# accounts/views.py
+# user_management/views.py
 
 from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth.models import User
-from .models import RecruiterProfile, ApplicantProfile
-from .serializers import RecruiterProfileSerializer, ApplicantProfileSerializer
 from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.response import Response
-
-class RecruiterSignupView(generics.CreateAPIView):
-    serializer_class = RecruiterProfileSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from .models import CustomUser, PostedJob
+import rest_framework.status as status
+from .serializers import ApplicantSignupSerializer, PostedJobSerializer, RecruiterSignupSerializer, UserLoginSerializer
 
 class ApplicantSignupView(generics.CreateAPIView):
-    serializer_class = ApplicantProfileSerializer
+    queryset = CustomUser.objects.all()
+    serializer_class = ApplicantSignupSerializer
+    permission_classes = [AllowAny]
+
+class RecruiterSignupView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RecruiterSignupSerializer
+    permission_classes = [AllowAny]
+
+class UserLoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LoginView(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        print("token")
+        
         token, created = Token.objects.get_or_create(user=user)
-        print(token)
+        
         return Response({
             'token': token.key,
-            'user_id': user.pk,
-            # 'user_type': user.recruiterprofile if hasattr(user, 'recruiterprofile') else 'applicant'
-        })
+            'usertype': user.usertype
+        }, status=status.HTTP_200_OK)
+
+
+class PostedJobView(generics.ListCreateAPIView):
+    queryset = PostedJob.objects.all()
+    serializer_class = PostedJobSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
