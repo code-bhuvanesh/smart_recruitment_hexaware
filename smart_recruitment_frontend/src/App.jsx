f3bc13d1d93da6ed9633b81825aeb572e83d6fd9
Bhuvanesh <90144561+code-bhuvanesh@users.noreply.github.com>
2024-09-22 15:26:47 +0530

    commit

--- smart_recruitment_frontend/src/App.jsx
+++ smart_recruitment_frontend/src/App.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import Sidebar from './Pages/Sidebar';
-import Dashboard from './Pages/Dashboard';
-import LoginPage from './Pages/LoginPage';
-import './App.css'; // Include your custom styles
-import Register from './Pages/Register';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import Sidebar from "./Pages/Sidebar";
+import Dashboard from "./Pages/Dashboard";
+import LoginPage from "./Pages/LoginPage";
+import "./App.css"; // Include your custom styles
+import Register from "./Pages/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-
     <Router>
-      <Sidebar />
       <Routes>
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<Register />} />
       </Routes>
